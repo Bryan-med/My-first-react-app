64487db6b76b422c04e15860c054a6a1a944d88d
Bryan Medeles <bryan@Bryans-MacBook-Air-1.local>
2026-09-06 18:27:23 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx' 
+import Coworkers from './Coworkers.jsx'
+
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Footer/>
+    <Coworkers/>
+    <body></body>
+</>
+  )
+  
+}
+
+export default App
---
+++ src/Coworkers.jsx
@@ -0,0 +1,21 @@
+function Coworkers() {
+const coworkers1 = "Paulina oreo ortega"
+const coworkers2 = "issac"
+const coworkers3 = "david"
+
+
+
+return( 
+<ul>
+    <h2> My Coworkers </h2>
+    <li>{coworkers1}</li>
+    <li>{coworkers2}</li>
+    <li>{coworkers3}</li>
+</ul>
+
+);
+
+
+}
+
+export default Coworkers
